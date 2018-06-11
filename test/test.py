de805c12bdfb0362a2e97117f04e92f76534dd77
Kerrin Mendler <mendlerke@gmail.com>
2018-06-11 18:35:36 -0400

    Fix test for wercker

--- test/test.py
+++ test/test.py
@@ -2,5 +2,4 @@ import subprocess
 
 
 def test_pipeline():
-    subprocess.call("cd ..")
-    subprocess.check_call(["snakemake", "-n"])
+    subprocess.check_call(["snakemake -s ../Snakefile", "-n"])
